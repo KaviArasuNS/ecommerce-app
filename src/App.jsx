ed76745d6c86ade34b1227e4caa9f845658708db
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-08-12 19:40:14 +0530

    ecom app

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import PorductList from "./pages/PorductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart"
+import {
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home/>
+    const user = true;
+  return(
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="/products/:category" element={<PorductList/>} />
+      <Route path="/product/:id" element={<Product/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/login" element={ user ? <Navigate replace to="/"/> : <Login/>} />
+      <Route path="/register" element={user ? <Navigate replace to="/" /> : <Register/>}/>
+    </Routes>
+  )
 };
 
 export default App;
--- src/pages/PorductList.jsx
+++ src/pages/PorductList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Navbar from "../components/Navbar"
 import Announcement from "../components/Announcement"
@@ -6,6 +6,7 @@ import Products from "../components/Products"
 import Newsletter from "../components/Newsletter"
 import Footer from "../components/Footer"
 import mobile    from "../responsive"
+import { useLocation } from 'react-router-dom'
 
 const Container = styled.div`
 
@@ -42,27 +43,40 @@ const Option = styled.option`
 `
 
 const PorductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  } 
+  
   return (
     <Container>
       <Navbar/>
       <Announcement/>
-      <Title>Dresses</Title>
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
         <FilterText>Filter Products</FilterText>
-        <Select>
-           <Option disabled selected>
+        <Select name="color" onChange={handleFilters}>
+           <Option disabled >
               Color
             </Option>
-            <Option>White</Option>
+            <Option>Cloud Dancer</Option>
             <Option>Black</Option>
             <Option>Red</Option>
-            <Option>Blue</Option>
-            <Option>Yellow</Option>
-            <Option>Green</Option>     
+            <Option>White</Option>
+            <Option>Grey</Option>
+            <Option>Orange</Option>     
         </Select>
-        <Select>
-            <Option disabled selected>
+        <Select name="size" onChange={handleFilters}>
+            <Option disabled >
               Size
             </Option>
             <Option>XS</Option>
@@ -74,14 +88,14 @@ const PorductList = () => {
         </Filter>
         <Filter>
             <FilterText>Sort Products</FilterText>
-             <Select>
-            <Option selected>Newest</Option>
-            <Option>Price (asc)</Option>
-            <Option>Price (desc)</Option>
+             <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
-      <Products/>
+      <Products cat={cat} filters={filters} sort={sort} />
       <Newsletter/>
       <Footer/>
     </Container>
