07423b0d6cacccfa93540710ca7fb0ee995aa331
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-08-28 23:46:15 +0530

    _redirects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import SearchIcon from '@mui/icons-material/Search';
 import { Badge } from '@mui/material';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import mobile  from "../responsive"
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
     height: 60px;
@@ -69,6 +71,9 @@ const MenuItem = styled.div`
 `
 
 const Navbar = () => {
+    const quantity = useSelector(state=>state.cart.quantity);
+
+    console.log(quantity);
   return (
     <Container>
         <Wrapper>
@@ -87,11 +92,13 @@ const Navbar = () => {
          <Right>
             <MenuItem>REGISTER</MenuItem>
             <MenuItem>SIGN IN</MenuItem>
+            <Link to="/cart">
             <MenuItem>
-                <Badge badgeContent={4} color="primary">
+                <Badge badgeContent={quantity} color="primary">
                     <ShoppingCartOutlinedIcon/>
                 </Badge>
             </MenuItem>
+            </Link>
          </Right>
         </Wrapper> 
     </Container>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import App from './App';
 import {
   BrowserRouter
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+     </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
