01cce47a47b4084737ea8b4bb8a58b4d407ac60f
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-09-21 11:07:25 +0530

    render url update

--- src/pages/PorductList.jsx
+++ src/pages/PorductList.jsx
@@ -1,46 +1,41 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Navbar from "../components/Navbar"
-import Announcement from "../components/Announcement"
-import Products from "../components/Products"
-import Newsletter from "../components/Newsletter"
-import Footer from "../components/Footer"
-import mobile    from "../responsive"
-import { useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import mobile from "../responsive";
+import { useLocation } from "react-router-dom";
 
-const Container = styled.div`
-
-`
+const Container = styled.div``;
 
 const Title = styled.h1`
-    margin: 20px;
-`
+  margin: 20px;
+`;
 const FilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    
-`
+  display: flex;
+  justify-content: space-between;
+`;
 const Filter = styled.div`
-    margin: 20px;
-    ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column"})}
-`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
 
 const FilterText = styled.span`
-    font-size: 20px;
-    font-weight: 600;
-    margin-right: 20px;
-    ${mobile({ marginRight: "0px"})}
-`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
+`;
 
 const Select = styled.select`
-    padding: 10px;
-    margin-right: 20px;
-    ${mobile({ margin: "10px 0px"})}
-`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
+`;
 
-const Option = styled.option`
-
-`
+const Option = styled.option``;
 
 const PorductList = () => {
   const location = useLocation();
@@ -54,41 +49,37 @@ const PorductList = () => {
       ...filters,
       [e.target.name]: value,
     });
-  } 
-  
+  };
+
   return (
     <Container>
-      <Navbar/>
-      <Announcement/>
+      <Navbar />
+      <Announcement />
       <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
-        <FilterText>Filter Products</FilterText>
-        <Select name="color" onChange={handleFilters}>
-           <Option disabled >
-              Color
-            </Option>
+          <FilterText>Filter Products</FilterText>
+          <Select name="color" onChange={handleFilters}>
+            <Option disabled>Color</Option>
             <Option>Cloud Dancer</Option>
             <Option>Black</Option>
             <Option>Red</Option>
             <Option>White</Option>
             <Option>Grey</Option>
-            <Option>Orange</Option>     
-        </Select>
-        <Select name="size" onChange={handleFilters}>
-            <Option disabled >
-              Size
-            </Option>
+            <Option>Orange</Option>
+          </Select>
+          <Select name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
             <Option>XS</Option>
             <Option>S</Option>
             <Option>M</Option>
             <Option>L</Option>
             <Option>XL</Option>
-          </Select>  
+          </Select>
         </Filter>
         <Filter>
-            <FilterText>Sort Products</FilterText>
-             <Select onChange={(e) => setSort(e.target.value)}>
+          <FilterText>Sort Products</FilterText>
+          <Select onChange={(e) => setSort(e.target.value)}>
             <Option value="newest">Newest</Option>
             <Option value="asc">Price (asc)</Option>
             <Option value="desc">Price (desc)</Option>
@@ -96,10 +87,10 @@ const PorductList = () => {
         </Filter>
       </FilterContainer>
       <Products cat={cat} filters={filters} sort={sort} />
-      <Newsletter/>
-      <Footer/>
+      <Newsletter />
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default PorductList
+export default PorductList;
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZWJiZmQ5MTIxODY1ZWZjYzU0OTY4NyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MDMwNTg2NSwiZXhwIjoxNjYwNTY1MDY1fQ.K10Wyron6-zZB1kWOEpbyMOzn_8ZWtOeSKCXi1vtmGg"
 
+const RENDER_URL = "https://ecom-api-whnv.onrender.com/api";
+
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZWJiZmQ5MTIxODY1ZWZjYzU0OTY4NyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MDMwNTg2NSwiZXhwIjoxNjYwNTY1MDY1fQ.K10Wyron6-zZB1kWOEpbyMOzn_8ZWtOeSKCXi1vtmGg";
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+  baseURL: RENDER_URL,
 });
 
-
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
-    header: { token: `Bearer ${TOKEN}` },
+  baseURL: RENDER_URL,
+  header: { token: `Bearer ${TOKEN}` },
 });
